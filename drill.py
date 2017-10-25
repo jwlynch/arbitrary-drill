6ece40af853565ece13fdf4f8767847aaa43fb1e
Jim Lynch <jim@jam.sessionsnet.org>
2017-10-25 10:14:37 -0700

    start to fill in main func: deal with program name

--- drill.py
+++ drill.py
@@ -3,14 +3,15 @@
 import sys
 from random import choice
 
-drill_seq = sys.argv[1:]
 def usage(progname):
     print("%s: Usage:" % progname)
     print("%s" % progname)
     print("%s (to print this usage message)" % (" " * len(progname)))
 
 def main(progname=None, args=None):
-    pass
+    if progname is None:
+        progname = "prog"
+
 
 # "barely enough" parsing for -b
 if drill_seq[0] == "-b":
