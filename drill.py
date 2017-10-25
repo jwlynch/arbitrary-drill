4cea1825b0c258a4f6d72322cf76723efbe97fe7
Jim Lynch <jim@jam.sessionsnet.org>
2017-10-25 10:19:04 -0700

    add main functionality to func main()

--- drill.py
+++ drill.py
@@ -15,25 +15,28 @@ def main(progname=None, args=None):
     if len(sys.argv) == 0:
         print("%s: too few arguments" % progname)
         usage(progname)
+    else: # 1 or more args
+        drill_seq = sys.argv[1:]
 
-# "barely enough" parsing for -b
-if drill_seq[0] == "-b":
-    drill_seq.remove("-b")
-    printBeginP = True
-else:
-    printBeginP = False
-
-input("press enter for next item: ")
-
-while True:
-    live_seq = drill_seq[:]
-
-    if printBeginP:
-        print("(start of new sequence)")
-
-    while live_seq:
-        nxt = choice(live_seq)
-        print(nxt)
-        live_seq.remove(nxt)
+        # "barely enough" parsing for -b
+        if drill_seq[0] == "-b":
+            drill_seq.remove("-b")
+            printBeginP = True
+        else:
+            printBeginP = False
 
         input("press enter for next item: ")
+
+        while True:
+            live_seq = drill_seq[:]
+
+            if printBeginP:
+                print("(start of new sequence)")
+
+            while live_seq:
+                nxt = choice(live_seq)
+                print(nxt)
+                live_seq.remove(nxt)
+
+                input("press enter for next item: ")
+
