2dc30b21b0b1c9dd991a2416222a8caa6f7589de
Jim Lynch <jim@jam.sessionsnet.org>
2017-10-25 09:59:07 -0700

    add usage message func, start to fill it in

--- drill.py
+++ drill.py
@@ -4,6 +4,11 @@ import sys
 from random import choice
 
 drill_seq = sys.argv[1:]
+def usage(progname):
+    print("%s: Usage:" % progname)
+    print("%s" % progname)
+    print("%s (to print this usage message)" % (" " * len(progname)))
+
 
 # "barely enough" parsing for -b
 if drill_seq[0] == "-b":
